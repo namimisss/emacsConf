bae2666ce9f4e990c90dc331e293c75a6456a79c
sakurabeaver <877435317@qq.com>
2022-03-03 22:17:06 +0800

    modify init.el

--- init.el
+++ init.el
@@ -28,7 +28,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (company-c-headers company cmake-mode solarized-theme auto-complete))))
+    (lsp-java company-c-headers company cmake-mode solarized-theme auto-complete))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
