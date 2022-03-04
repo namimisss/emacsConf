2cd62ae983c785382bd9261403a04e65cd4e777a
sakurabeaver <877435317@qq.com>
2022-03-04 22:13:32 +0800

    add eglot

--- init.el
+++ init.el
@@ -12,7 +12,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (company-c-headers company use-package))))
+ '(package-selected-packages (quote (eglot company-c-headers company use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
